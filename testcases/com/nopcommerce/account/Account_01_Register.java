5861541e4cc1bc9dc3af8c85969cad2e27380815
dat.tran <dattran0808@gmail.com>
2024-08-02 16:13:31 +0700

    Framework 02 - Don't Repeat Yourself

--- testcases/com/nopcommerce/account/Account_01_Register.java
+++ testcases/com/nopcommerce/account/Account_01_Register.java
@@ -1,43 +1,110 @@
 package com.nopcommerce.account;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.Random;
+
 public class Account_01_Register {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
 
     @BeforeClass
     public void beforeClass() {
-
+        System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
     }
 
     @Test
     public void Register_01_Empty_Data() {
+        driver.get("https://demo.nopcommerce.com/");
+        driver.findElement(By.xpath("//a[@class='ico-register']")).click();
 
+        driver.findElement(By.xpath("//button[@id='register-button']")).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@id='FirstName-error']")).getText(), "First name is required.");
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@id='LastName-error']")).getText(), "Last name is required.");
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@id='Email-error']")).getText(), "Email is required.");
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@id='ConfirmPassword-error']")).getText(), "Password is required.");
     }
 
     @Test
     public void Register_02_Invalid_Email() {
+        driver.get("https://demo.nopcommerce.com/");
+        driver.findElement(By.xpath("//a[@class='ico-register']")).click();
 
+        driver.findElement(By.xpath("//input[@id='FirstName']")).sendKeys("abc");
+        driver.findElement(By.xpath("//input[@id='LastName']")).sendKeys("def");
+        driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("abc@def@gh");
+        driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("123456");
+        driver.findElement(By.xpath("//input[@id='ConfirmPassword']")).sendKeys("123456");
+
+        driver.findElement(By.xpath("//button[@id='register-button']")).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@id='Email-error']")).getText(), "Please enter a valid email address.");
     }
 
     @Test
     public void Register_03_Invalid_Password() {
+        driver.get("https://demo.nopcommerce.com/");
+        driver.findElement(By.xpath("//a[@class='ico-register']")).click();
 
+        driver.findElement(By.xpath("//input[@id='FirstName']")).sendKeys("abc");
+        driver.findElement(By.xpath("//input[@id='LastName']")).sendKeys("def");
+        driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("abc@gmail.com");
+        driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("123");
+        driver.findElement(By.xpath("//input[@id='ConfirmPassword']")).sendKeys("123");
+
+        driver.findElement(By.xpath("//button[@id='register-button']")).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@class='field-validation-error']")).getText(), "<p>Password must meet the following rules: </p><ul><li>must have at least 6 characters and not greater than 64 characters</li></ul>");
     }
 
     @Test
     public void Register_04_Incorrect_Confirm_Password() {
+        driver.get("https://demo.nopcommerce.com/");
+        driver.findElement(By.xpath("//a[@class='ico-register']")).click();
 
+        driver.findElement(By.xpath("//input[@id='FirstName']")).sendKeys("abc");
+        driver.findElement(By.xpath("//input[@id='LastName']")).sendKeys("def");
+        driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("abc@gmail.com");
+        driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("123456");
+        driver.findElement(By.xpath("//input[@id='ConfirmPassword']")).sendKeys("123457");
+
+        driver.findElement(By.xpath("//button[@id='register-button']")).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@id='ConfirmPassword-error']")).getText(), "The password and confirmation password do not match.");
     }
 
     @Test
     public void Register_05_Success() {
+        driver.get("https://demo.nopcommerce.com/");
+        driver.findElement(By.xpath("//a[@class='ico-register']")).click();
 
+        driver.findElement(By.xpath("//input[@id='FirstName']")).sendKeys("abc");
+        driver.findElement(By.xpath("//input[@id='LastName']")).sendKeys("def");
+        driver.findElement(By.xpath("//input[@id='Email']")).sendKeys(getEmailRandom());
+        driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("123456");
+        driver.findElement(By.xpath("//input[@id='ConfirmPassword']")).sendKeys("123456");
+
+        driver.findElement(By.xpath("//button[@id='register-button']")).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='result']")).getText(), "Your registration completed");
     }
 
     @AfterClass
     public void afterClass() {
+        driver.quit();
+    }
 
+    public String getEmailRandom() {
+        Random rand = new Random();
+        return "abc" + rand.nextInt(99999) + "@gmail.com";
     }
 }
